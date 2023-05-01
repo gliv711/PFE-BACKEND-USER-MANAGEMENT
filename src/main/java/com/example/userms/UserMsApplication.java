6cfaccfbe573da187e9d2419e845cccfac328a7e
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-01 22:52:47 +0200

    Update UserMsApplication.java

--- src/main/java/com/example/userms/UserMsApplication.java
+++ src/main/java/com/example/userms/UserMsApplication.java
@@ -66,9 +66,94 @@ public class UserMsApplication {
                 "FSB"
         );
 
+        User u2 = new User(
+                null,
+                "johndoe@gmail.com",
+                "456",
+                "123 Main St",
+                "555-1234",
+                "Utilisateur",
+                null,
+                "John",
+                "Doe",
+                "Marketing",
+                "New York",
+                new Date(),
+                new Date(),
+                new Date(),
+                new Date(),
+                new Date(),
+                "NYU"
+        );
+
+        User u3 = new User(
+                null,
+                "jane.smith@gmail.com",
+                "789",
+                "456 Park Ave",
+                "555-5678",
+                "Entreprise",
+                null,
+                "Jane",
+                "Smith",
+                "Finance",
+                "London",
+                new Date(),
+                new Date(),
+                new Date(),
+                new Date(),
+                new Date(),
+                "LSE"
+        );
+        User u4 = new User(
+                null,
+                "janedoe@example.com",
+                "password456",
+                "456 Elm St",
+                "555-987-6543",
+                "Entreprise",
+                null,
+                "Jane",
+                "Doe",
+                "Finance",
+                "Los Angeles",
+                new Date(2022, 8, 1),
+                new Date(2023, 7, 31),
+                new Date(2022, 8, 15),
+                new Date(2023, 7, 15),
+                new Date(),
+                "UCLA"
+        );
+
+        User u5 = new User(
+                null,
+                "bobsmith@example.com",
+                "password789",
+                "789 Oak St",
+                "555-555-5555",
+                "Administrateur",
+                null,
+                "Bob",
+                "Smith",
+                "Human Resources",
+                "Chicago",
+                new Date(2022, 6, 1),
+                new Date(2023, 5, 31),
+                new Date(2022, 7, 15),
+                new Date(2023, 5, 15),
+                new Date(),
+                "UIC"
+        );
 
         return args -> {
             UserRepository.save(u1);
+            UserRepository.save(u2);
+            UserRepository.save(u3);
+            UserRepository.save(u4);
+            UserRepository.save(u5);
+
+
+
         };
     }
 
