a7c9f47790b6d292b0135ac5a8d8bd4838218653
Neder Mfarrej <76585797+gliv711@users.noreply.github.com>
2023-05-31 17:58:53 +0200

    admin

--- src/main/java/com/example/userms/controller/UserController.java
+++ src/main/java/com/example/userms/controller/UserController.java
@@ -67,6 +67,12 @@ public class UserController {
 
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+    @GetMapping("/admin/all")
+    public List<Admin> getAllAdmins() {
+
+        return adminService.getAllA();
+
+    }
 
 
     @PostMapping("/user")
